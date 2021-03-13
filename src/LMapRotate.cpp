172e5e6f07b20a59dd7ffd2e6b0bf7d6dfec3d2a
keyzd <keyzd at yandex dot ru>
2021-03-13 16:59:20 +0500

    .

--- src/LMapRotate.cpp
+++ src/LMapRotate.cpp
@@ -1,8 +1,8 @@
 #include "LMapRotate.hpp"
 
 LMapRotateX::LMapRotateX( const float thetaDeg ) {
-	float sinTheta = sin( DEG2RAD( thetaDeg ) );
-	float cosTheta = cos( DEG2RAD( thetaDeg ) );
+	const float sinTheta = sin( DEG2RAD( thetaDeg ) );
+	const float cosTheta = cos( DEG2RAD( thetaDeg ) );
 
 	m[0] = Vec4( 1, 0, 0, 0 );
 	m[1] = Vec4( 0, cosTheta, sinTheta, 0 );
@@ -13,8 +13,8 @@ LMapRotateX::LMapRotateX( const float thetaDeg ) {
 }
 
 LMapRotateY::LMapRotateY( const float thetaDeg ) {
-	float sinTheta = sin( DEG2RAD( thetaDeg ) );
-	float cosTheta = cos( DEG2RAD( thetaDeg ) );
+	const float sinTheta = sin( DEG2RAD( thetaDeg ) );
+	const float cosTheta = cos( DEG2RAD( thetaDeg ) );
 
 	m[0] = Vec4( cosTheta, 0, -sinTheta, 0 );
 	m[1] = Vec4( 0, 1, 0, 0 );
@@ -25,8 +25,8 @@ LMapRotateY::LMapRotateY( const float thetaDeg ) {
 }
 
 LMapRotateZ::LMapRotateZ( const float thetaDeg ) {
-	float sinTheta = sin( DEG2RAD( thetaDeg ) );
-	float cosTheta = cos( DEG2RAD( thetaDeg ) );
+	const float sinTheta = sin( DEG2RAD( thetaDeg ) );
+	const float cosTheta = cos( DEG2RAD( thetaDeg ) );
 
 	m[0] = Vec4( cosTheta, sinTheta, 0, 0 );
 	m[1] = Vec4( -sinTheta, cosTheta, 0, 0 );
